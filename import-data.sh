df594f6256ef894b6fd39c4b4096e8d393fc104c
Brian McClune <bpmcclune@gmail.com>
2022-07-19 08:43:50 -0400

    Change path to JSON resources

--- import-data.sh
+++ import-data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 elasticdump \
-    --input=document_mapping.json \
+    --input=/tmp/document_mapping.json \
     --output=http://localhost:9200/document \
     --type=mapping
 elasticdump \
-    --input=document.json \
+    --input=/tmp/document.json \
     --output=http://localhost:9200/document \
     --type=data
